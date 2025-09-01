a94c2ee95d7c34a73919e27b0a96ad44e72543fe
agusbrocani <aguss.brocani@gmail.com>
2025-09-01 12:28:29 -0300

    feat: implemento ingreso de parámetros

--- APL/bash/ejercicio5.sh
+++ APL/bash/ejercicio5.sh
@@ -15,3 +15,86 @@
 #        VARALDO, RODRIGO           42772765            #
 #                                                       #
 #-------------------------------------------------------#
+
+# Colores ANSI
+readonly RED='\e[31m'
+readonly GREEN='\e[32m'
+readonly YELLOW='\e[33m'
+readonly BLUE='\e[34m'
+readonly RESET='\e[0m'
+
+# Constantes
+readonly MAX_TTL=86400
+
+# Funciones de salida
+function info() {
+  printf "${BLUE}%-8s${RESET}%s\n" "[INFO]" "$1"
+}
+
+function success() {
+  printf "${GREEN}%-8s${RESET}%s\n" "[OK]" "$1"
+}
+
+function warn() {
+  printf "${YELLOW}%-8s${RESET}%s\n" "[WARN]" "$1"
+}
+
+function error() {
+  printf "${RED}%-8s${RESET}%s\n" "[ERROR]" "$1" >&2
+  exit 1
+}
+
+# Valores por defecto
+TTL=3600
+DROP_CACHE=false
+NOMBRES=()
+
+# Parseo de argumentos
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -n|--nombre)
+      shift
+      while [[ $# -gt 0 && ! "$1" =~ ^- ]]; do
+        NOMBRES+=("$1")
+        shift
+      done
+      ;;
+    -t|--ttl)
+      shift
+      if ! [[ "$1" =~ ^[0-9]+$ ]]; then
+        error "El TTL debe ser un número entero"
+      elif (( $1 < 0 || $1 > MAX_TTL )); then
+        error "El TTL debe estar entre 0 y ${MAX_TTL}"
+      fi
+      TTL="$1"
+      shift
+      ;;
+    -d|--dropCacheFile)
+      DROP_CACHE=true
+      shift
+      ;;
+    -*)
+      error "Parámetro desconocido: $1"
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+# Validaciones obligatorias
+if [[ ${#NOMBRES[@]} -eq 0 ]]; then
+  error "Debe especificar al menos un nombre con -n o --nombre"
+fi
+
+
+#! BORRAR AL FINAL
+    success "Esto es una prueba de success"
+    info "Esto es una prueba de info"
+    warn "Esto es una advertencia"
+
+    info "Parámetros recibidos:"
+    echo "- Nombres: ${NOMBRES[*]}"
+    echo "- TTL: $TTL"
+    echo "- Drop Cache: $DROP_CACHE"
+#!
